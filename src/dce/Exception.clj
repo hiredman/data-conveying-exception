8a659b82a226caff07034313e6d859d8f7a56cdc
Kevin Downey (hiredman) <redchin@gmail.com>
2011-05-05 09:36:28 -0700

    init commit

---
+++ src/dce/Exception.clj
@@ -0,0 +1,19 @@
+(ns dce.Exception
+  (:gen-class :extends RuntimeException
+              :state payload
+              :init init
+              :constructors {[String Throwable clojure.lang.Keyword
+                              java.util.Map]
+                             [String Throwable]
+                             [String clojure.lang.Keyword java.util.Map]
+                             [String]
+                             [clojure.lang.Keyword java.util.Map]
+                             []}))
+
+(defn -init
+  ([message cause type data]
+     [[message cause] (vary-meta data assoc :type type)])
+  ([message type data]
+     [[message] (vary-meta data assoc :type type)])
+  ([type data]
+     [[] (vary-meta data assoc :type type)]))
