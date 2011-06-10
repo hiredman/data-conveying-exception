eb775f1f38ddb19a239dd2a78a28afcb377a99b0
Phil Hagelberg <technomancy@gmail.com>
2011-06-09 17:28:34 -0700

    Implement IPersistentMap, j.u.Map, and IFn.

--- src/dce/Exception.clj
+++ src/dce/Exception.clj
@@ -1,20 +1,71 @@
 (ns dce.Exception
   (:gen-class :extends RuntimeException
-              :implements [clojure.lang.ILookup]
-              :state data
+              :implements [clojure.lang.IPersistentMap
+                           clojure.lang.MapEquivalence
+                           clojure.lang.IFn
+                           java.util.Map]
+              :state state
               :init init
               :constructors {[java.util.Map]
                              [String Throwable]}))
 
 (defn -init
-  ([{:keys [message cause] :as data}]
-     [[message cause] data]))
+  ([{:keys [message cause] :as state}]
+     [[message cause] state]))
+
+(defn -toString [self]
+  (.toString (.state self)))
+
+;; ILookup
 
 (defn -valAt
   ([self key]
-     (.valAt (.data self) key))
+     (.valAt (.state self) key))
   ([self key not-found]
-     (.valAt (.data self) key not-found)))
+     (.valAt (.state self) key not-found)))
 
-(defn -toString [e]
-  (.toString (.data e)))
+;; Seqable
+
+(defn -seq [self]
+  (.seq (.state self)))
+
+;; Associative
+
+(defn -containsKey [self key]
+  (.containsKey (.state self)))
+
+(defn -entryAt [self key]
+  (.entryAt (.state self)))
+
+(defn -assoc [self key val]
+  (dce.Exception. (assoc (.state self) key val)))
+
+;; IPersistentMap
+
+(defn -without [self key]
+  (dissoc (.state self) key))
+
+;; IPersistentCollection
+
+(defn -cons [self o]
+  (dce.Exception. (.cons (.state self) o)))
+
+(defn -empty [self]
+  (dce.Exception. {}))
+
+(defn -equiv [self o]
+  (.equiv (.state self) (.state o)))
+
+;; Iterable
+
+(defn -iterator [self]
+  (.iterator (.state self)))
+
+;; IFn
+
+(def -invoke -valAt)
+
+;; Counted
+
+(defn -count [self]
+  (count (.state self)))
--- test/dce/test/exception.clj
+++ test/dce/test/exception.clj
@@ -1,12 +1,41 @@
 (ns dce.test.exception
   (:use [clojure.test]))
 
+(def e (dce.Exception. {:a :bob :b :alice}))
+
 (deftest test-constructing
-  (is (instance? dce.Exception (dce.Exception. {}))))
+  (is (instance? dce.Exception e)))
 
 (deftest test-get
-  (is (:data-conveying (dce.Exception. {:data-conveying true}))))
+  (is (:a e)))
 
 (deftest test-str
-  (is (= (str {:data-conveying true})
-         (str (dce.Exception. {:data-conveying true})))))
+  (is (= "{:a :bob, :b :alice}" (str e))))
+
+;; TODO: implementing ISeq breaks this; works with just ILookup.
+(deftest test-destructure
+  (is (= [:bob :alice]
+         (let [{:keys [a b]} e]
+           [a b]))))
+
+(deftest test-catching
+  (is
+   (try
+     (when true
+       (throw e))
+     (catch Exception x
+       (:a x)))))
+
+(deftest test-keys-vals
+  (is (= [:a :b] (keys e)))
+  (is (= [:bob :alice] (vals e))))
+
+(deftest test-assoc-dissoc
+  (is (= :charlie (:c (assoc e :c :charlie))))
+  (is (= [:b] (keys (dissoc e :a)))))
+
+(deftest test-count
+  (is (= 2 (count e))))
+
+(deftest test-ifn
+  (is (= :bob (e :a))))
