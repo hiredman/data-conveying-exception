152777c64f126a717e9fe7e50159a91ecde7951d
Phil Hagelberg <technomancy@gmail.com>
2011-06-10 13:55:47 -0700

    Bump version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dataconveyingexception "0.0.2"
+(defproject dataconveyingexception "0.0.3-SNAPSHOT"
   :description "a DCE"
   :dependencies [[org.clojure/clojure "1.2.1"]]
   :aot [dce.Exception])
